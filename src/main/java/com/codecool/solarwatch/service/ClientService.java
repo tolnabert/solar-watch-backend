4db5daea1288f0226085e3593dbbf1b326be5586
tolnabert <tolnabert@gmail.com>
2024-08-01 15:54:36 +0200

    adds CI WORKS for test admin page method

--- src/main/java/com/codecool/solarwatch/service/ClientService.java
+++ src/main/java/com/codecool/solarwatch/service/ClientService.java
@@ -81,7 +81,7 @@ public class ClientService {
 
     public String getAdminUsername() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return "Admin Endpoint Accessed by admin: " + user.getUsername();
+        return "CI works\nAdmin Endpoint Accessed by admin: " + user.getUsername();
     }
 
     public String getUserUsername() {
