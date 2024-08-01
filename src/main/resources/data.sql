98a6f89a526b27efdfc27f474305e3991fd75e72
tolnabert <tolnabert@gmail.com>
2024-08-01 17:38:36 +0200

    updated the credentiels for admin and user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- ADMIN
 INSERT INTO client (public_id, first_name, last_name, date_of_birth, email, username, password)
 VALUES ('d4f8915a-599d-4dd3-a8c3-034f7fa946c4', 'admin', 'test', '2000-01-01', 'admin@test.com', 'admin',
-        '$2a$10$efNGeid3i4kzYnswxSA7j.9wHLN3FZFtgm0/UOvcAaJ9rUhXSKkDq');
+        '$2a$10$bOlA1.fY49cYPf0y3kmr5edADOD5vCAuU9iJObAS2SLb4AmyhDKv.');
 -- ADMIN ROLE
 INSERT INTO client_roles (client_id, role)
 SELECT id, 'ROLE_ADMIN'
@@ -15,8 +15,8 @@ WHERE public_id = 'd4f8915a-599d-4dd3-a8c3-034f7fa946c4';
 
 -- CLIENT
 INSERT INTO client (public_id, first_name, last_name, date_of_birth, email, username, password)
-VALUES ('604d1d39-55ff-4abb-9d97-78e6125c3dd8', 'user', 'test', '2000-01-01', 'user@test.com', 'user',
-        '$2a$10$efNGeid3i4kzYnswxSA7j.9wHLN3FZFtgm0/UOvcAaJ9rUhXSKkDq');
+VALUES ('604d1d39-55ff-4abb-9d97-78e6125c3dd8', 'user', 'test', '2000-01-01', 'testuser@test.com', 'testuser',
+        '$2a$10$bOlA1.fY49cYPf0y3kmr5edADOD5vCAuU9iJObAS2SLb4AmyhDKv.');
 
 -- USER ROLE
 INSERT INTO client_roles (client_id, role)
